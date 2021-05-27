7cfd75c297279e6035c3085920e497c4a5525bc7
Danish shaikh <46679276+danishshaikh4499@users.noreply.github.com>
2021-05-27 23:58:43 +0530

    Add files via upload

---
+++ Day-2_26-05-21.php
@@ -0,0 +1,16 @@
+<?php
+$a = 8;
+$d = 10;
+//method 1
+echo"First value is ".$a;
+echo"</br> Second value is ".$d;
+//method 2
+echo"<br/>First value is ".$a."</br> Second value is ".$d;
+//method 3
+echo"<br/>First value is $a</br> Second value is $d";
+//method 4
+echo'<br/>First value is $a</br> Second value is $d';
+//method 5
+echo"<br/>First value is $a</br> Second value is $d</br>Sum is ".($a+$d);
+
+?>
