bf1c126ff57a687e34964df9b16c0df10a1558b8
Danish shaikh <46679276+danishshaikh4499@users.noreply.github.com>
2021-05-28 00:28:50 +0530

    Add files via upload

---
+++ 03-Day-2_26-05-21.php
@@ -0,0 +1,23 @@
+<?php
+$a = 3;
+switch($a)
+{
+    case 1:
+            echo"The number is 1.";
+            break;
+    case 2:
+            echo"The number is 2.";
+            break;
+    case 3:
+            echo"The number is 3.";
+            break;
+    case 4:
+            echo"The number is 4.";
+            break;
+    default:
+            echo"Please select a number between 1 to 4.";
+            
+}
+echo"</br>Thank You";
+
+?>
