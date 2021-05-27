c11fd44f6812c8fbbd7e8c266160426b1a3d2b0e
Danish shaikh <46679276+danishshaikh4499@users.noreply.github.com>
2021-05-28 00:07:57 +0530

    Add files via upload

---
+++ 02-Day-2_26-05-21.php
@@ -0,0 +1,11 @@
+<?php
+$a = 5;
+$d = 10;
+//Sum
+echo"<br/>First value is $a</br> Second value is $d</br>Sum is ".($a+$d);
+//Multiplication
+echo"</br>Multiplication is ".($a*$d);
+//Division
+echo"</br>Division is ".($d/$a);
+
+?>
